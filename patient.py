f9970084e7f7d332ad730b1a913a178464fd5717
jtuttlelbh <39383655+jtuttlelbh@users.noreply.github.com>
2018-06-15 10:42:15 -0400

    Add files via upload

---
+++ patient.py
@@ -0,0 +1,8 @@
+class Patient:
+    def __init__(self, **kwargs):
+        self.firstName = str(kwargs.pop('FirstName')).title()
+        self.middleName = str(kwargs.pop('MiddleName')).title()
+        self.lastName = str(kwargs.pop('LastName')).title()
+        self.email = kwargs.pop('Email')
+        self.validEmail = kwargs.pop('ValidEmail')
+        self.appointment = kwargs.pop('Appointment')
